8d74307baead25001dfc6800d696aa0be42a9017
Jordy <jordyfgomes@gmail.com>
2016-07-10 18:39:05 -0300

    Agora o app lế o arquivo config.json para pegar o servidor e coloquei um makefile para automatizar o processo de exportar e abrir o programa

--- client-NoSpotfy/js/services/enums.js
+++ client-NoSpotfy/js/services/enums.js
@@ -1,4 +1,11 @@
-var HOST = "localhost";
+var path = require('path');
+var fs   =  require('fs');
+
+var appDir = path.dirname(require.main.filename);
+var jsonFile =  fs.readFileSync(appDir + path.sep + "config.json");
+var json = JSON.parse(jsonFile);
+
+var HOST = json.server;
 var PORT_SEND = "53105";
 var PORT_RCVS = "7331";
 var PORT_SERV_WEB = "8080";
