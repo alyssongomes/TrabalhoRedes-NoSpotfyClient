6d573fbf2afce3bfab2e87274644e42d1f35b86e
Jordy <jordyfgomes@gmail.com>
2016-07-10 23:40:13 -0300

    hotfix

--- client-NoSpotfy/js/services/enums.js
+++ client-NoSpotfy/js/services/enums.js
@@ -1,11 +1,16 @@
 var path = require('path');
 var fs   =  require('fs');
+var host = sessionStorage.getItem('server');
+if(host == undefined)
+{
+  var appDir = path.dirname(require.main.filename);
+  var jsonFile =  fs.readFileSync(appDir + path.sep + "config.json");
+  var json = JSON.parse(jsonFile);
+  sessionStorage.setItem('server',json.server);
+  host = json.server;
+}
 
-var appDir = path.dirname(require.main.filename);
-var jsonFile =  fs.readFileSync(appDir + path.sep + "config.json");
-var json = JSON.parse(jsonFile);
-
-var HOST = json.server;
+var HOST = host;
 var PORT_SEND = "53105";
 var PORT_RCVS = "7331";
 var PORT_SERV_WEB = "8080";
